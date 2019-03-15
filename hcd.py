5eda8106110476ac67431600735e052ebe19550a
abhina52 <abhina52@hotmail.com>
2019-03-15 19:39:15 -0400

    added training code, evaluation code

--- hcd.py
+++ hcd.py
@@ -6,7 +6,10 @@ import torch.nn.functional as F
 import numpy as np
 import torchvision.transforms as transforms
 import torch.optim as optim
-
+import os
+import os.path
+from random import shuffle
+from torch.autograd import Variable
 
 def get_features ():
     alexNet = AlexNetFeatures()
@@ -51,7 +54,7 @@ class SmallNet(nn.Module):
         self.name = "small"
         self.conv = nn.Conv2d(256, 512, 3)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc = nn.Linear(128 * 2 * 2, 9)
+        self.fc = nn.Linear(128 * 2 * 2, 2)
     def forward(self, x):
         x = self.pool(F.relu(self.conv(x)))
         x = self.pool(x)
@@ -60,10 +63,115 @@ class SmallNet(nn.Module):
         x = x.squeeze(1) # Flatten to [batch_size]
 
         return x
-print("START")
 
-get_features()
+#get_features()
 
-print("DONE")
+def load_features(dir, label):
+    dir = os.path.expanduser(dir)
+    tensors = []
+    for target in sorted(os.listdir(dir)):
+        d = os.path.join(dir, target)
+        print (d)
+        tensors.append((torch.load(d), label))
+    return tensors
+
+def evaluate(net, valid, criterion):
+    total_loss = 0.0
+    total_err = 0.0
+
+    for i in range(len(valid)):
+        optimizer.zero_grad()
+        feature,label = train[i]
+        outputs = torch.zeros(70, 1, 2)
+        prediction = 0
+        lossOutput = torch.zeros(1,2)
+        for j in range(len(feature)):
+            outputs[j] = (net(feature[j]))
+            if (outputs.max(1, keepdim=True) == 1):
+                prediction = 1
+                lossOutput = outputs[j]
+        if (prediction != 1):
+            lossOutput = torch.mean(outputs, dim=0, keepdim=True)
+        total_err += prediction != label.item()
+        total_loss += lossOutput.item()
+
+    err = float(total_err)/len(valid)
+    loss = float(total_loss)/len(valid)
+
+    return err, loss
+        
+
+    
+
+def train_net(net, train, valid, learning_rate=0.001, weight_decay = 0, num_epochs=10):
+    torch.manual_seed(1000)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate, weight_decay = weight_decay)
+
+    train_err = np.zeros(num_epochs)
+    train_loss = np.zeros(num_epochs)
+    val_err = np.zeros(num_epochs)
+    val_loss = np.zeros(num_epochs)
+
+    for epoch in range(num_epochs):
+        shuffle(train)
+        total_train_loss = 0.0
+        total_train_err = 0.0
+
+        total_epoch = 0
+        
+        for i in range(len(train)):
+            optimizer.zero_grad()
+            feature,label = train[i]
+            #print (feature[0].shape, label)
+            #target = Variable(torch.Tensor(label).long())
+            #print (label.shape)
+            outputs = torch.zeros(70, 1, 2)
+            prediction = 0
+            lossOutput = torch.zeros(1,2)
+            for j in range(len(feature)):
+                outputs[j] = (net(feature[j]))
+                if (outputs.max(1, keepdim=True) == 1):
+                    prediction = 1
+                    lossOutput = outputs[j]
+            if (prediction != 1):
+                lossOutput = torch.mean(outputs, dim=0, keepdim=True)
+
+            loss = criterion(lossOutput.squeeze(0), label)
+            loss.backward()
+            optimizer.step()
+
+            total_train_err += (prediction != label.item())
+            total_train_loss +=loss.item()
+            total_epoch += 1
+
+        train_err[epoch] = float(total_train_err)/total_epoch
+        train_loss[epoch] = float(total_train_loss)/(i + 1)
+        val_err[epoch], val_loss[epoch] = evaluate(net, valid, criterion)
+        
+        print(("Epoch {}: Train err: {}, Train loss: {}").format(
+            epoch + 1,
+            train_err[epoch],
+            train_loss[epoch],
+            val_err[epoch],
+            val_loss[epoch]))
+            
+            
+            
+#non_cancerous = [[1,0]]
+cancerous = Variable(torch.Tensor([1]).long())
+non_cancerous = Variable(torch.Tensor([0]).long())
+
+
+#print (non_cancerous.shape)
+            
+train = load_features('Train_Alexnet_features_cancerous', cancerous)
+train.extend(load_features('Train_Alexnet_features_non_cancerous', non_cancerous))
+
+valid = load_features('Valid_Alexnet_features_cancerous', cancerous)
+valid.extend(load_features('Valid_Alexnet_features_non_cancerous', non_cancerous))
+
+smallnet = SmallNet()
+train_net(smallnet, train, valid)
 
 
