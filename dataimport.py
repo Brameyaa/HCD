7c4ca1301d6edd8c3da3b653630f8a74672961fb
abhina52 <abhina52@hotmail.com>
2019-03-15 12:56:47 -0400

    adding data_import, alex net features

--- dataimport.py
+++ dataimport.py
@@ -2,7 +2,7 @@
 from skimage import io
 import numpy as np
 
-im = io.imread() #per tif image, converts to numpy array
+#im = io.imread() #per tif image, converts to numpy array
 
 #-----Potential Method 2--------
 import torch
@@ -10,11 +10,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from torchvision import transforms, datasets
+import os
+import os.path
 
-data_training = datasets.ImageFolder(root='Training_data',
-                                     transform=transforms.ToTensor())
-data_test = datasets.ImageFolder(root='Test_data',
-                                     transform=transforms.ToTensor())
+#data_training = datasets.ImageFolder(root='Training_data',
+#                                     transform=transforms.ToTensor())
+#data_test = datasets.ImageFolder(root='Test_data',
+#                                     transform=transforms.ToTensor())
 
 
 from torch.utils import data
@@ -34,42 +36,70 @@ def make_dataset(dir, label, extensions, transform):
   #Image Size: 2048 x 1536 pixels
    images = []
    labels = []
+
+   
    dir = os.path.expanduser(dir)
+
    for target in sorted(os.listdir(dir)):
      d = os.path.join(dir, target)
      filename, ext = os.path.splitext(os.path.basename(d))
      filename = filename.split('_')
+     print (dir, filename, ext)
      img = io.imread(d) #plt.imread(d)[:, :, :3]
-     x = transform(img)
-     label = filename[2]
+     img = img.astype(np.float32)
+
+     x = torch.from_numpy(img) #transform(img)
+     label = label
+     #print (filename, label)
      x = x.permute(2,0,1)
-     result = np.zeros((3, 2240, 1568))
-     result[:x.shape[0],:x.shape[1]] = x
+     result = torch.zeros((3, 1568, 2240))
+   
+     result[:, :x.shape[1],:x.shape[2]] = x
      y = []
-     for i in range(10):
-        for j in range(7):
-          y.append(x[:, i * 224: (i + 1) * 224, j * 224: (j + 1) * 224]
-     y = y.reshape([54,3,224,224])
+     for i in range(7):
+        for j in range(10):
+          y.append(result[:, i * 224: (i + 1) * 224, j * 224: (j + 1) * 224])
      #x = x.reshape([3, 224, 224])
      images.append(y)
      labels.append(label)
+
    return images, np.array(labels)
    
 
-  def importImages ():
+def importImages ():
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     
     train_img1, train_labels1 = make_dataset('Training_data/Normal', 0, '.tif', transform)
     train_img2, train_labels2 = make_dataset('Training_data/Benign', 0, '.tif', transform)
     train_img3, train_labels3 = make_dataset('Training_data/In Situ', 1, '.tif', transform)
     train_img4, train_labels4 = make_dataset('Training_data/Invasive', 1, '.tif', transform)
-    
+
+    valid_img1, valid_labels1 = make_dataset('Validation_data/Normal', 0, '.tif', transform)
+    valid_img2, valid_labels2 = make_dataset('Validation_data/Benign', 0, '.tif', transform)
+    valid_img3, valid_labels3 = make_dataset('Validation_data/In Situ', 1, '.tif', transform)
+    valid_img4, valid_labels4 = make_dataset('Validation_data/Invasive', 1, '.tif', transform)
+
+    test_img1, test_labels1 = make_dataset('Test_data/Normal', 0, '.tif', transform)
+    test_img2, test_labels2 = make_dataset('Test_data/Benign', 0, '.tif', transform)
+    test_img3, test_labels3 = make_dataset('Test_data/In Situ', 1, '.tif', transform)
+    test_img4, test_labels4 = make_dataset('Test_data/Invasive', 1, '.tif', transform)
+
     train_img = []
     train_label = []
     train_img.extend([train_img1, train_img2, train_img3, train_img4])
-    train_label.extend([train_label1, train_label2, train_label3, train_label4])
+    train_label.extend([train_labels1, train_labels2, train_labels3, train_labels4])
+
+    valid_img = []
+    valid_label = []
+    valid_img.extend([valid_img1, valid_img2, valid_img3, valid_img4])
+    valid_label.extend([valid_labels1, valid_labels2, valid_labels3, valid_labels4])
+    
+    test_img = []
+    test_label = []
+    test_img.extend([test_img1, test_img2, test_img3, test_img4])
+    test_label.extend([test_labels1, test_labels2, test_labels3, test_labels4])
     
     
-    return train_img, train_label
+    return train_img, train_label, valid_img, valid_label, test_img, test_label
     
   
--- hcd.py
+++ hcd.py
@@ -1,46 +1,64 @@
 from a3code import AlexNetFeatures
+from dataimport import importImages
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import torchvision.transforms as transforms
+import torch.optim as optim
+
 
 def get_features ():
     alexNet = AlexNetFeatures()
     alexNet.eval()
   
-  train_img, train_label = importImages()
-  
-  train_features = []
-  valid_features = []
-  test_features = []
-  for i in range (len(train_img)):
-    x = []
-    for j in range(70):
-         x.append(alexNet(train_img[i][j].reshape([1, 3, 224, 224])))
-    train_features.append(x)
-   torch.save(train_features, 'train_features.pt')
-  for i in range (len(valid_img)):
-    x = []
-    for j in range(70):
-         x.append(alexNet(valid_img[i][j].reshape([1, 3, 224, 224])))
-    valid_features.append(x)
-  torch.save(valid_features, 'valid_features.pt')
-  for i in range (len(test_img)):
-    x = []
-    for j in range(70):
-         x.append(alexNet(test_img[i][j].reshape([1, 3, 224, 224])))
-    test_features.append(x)
-  torch.save(test_features, 'test_features.pt')
+    train_img, train_label, valid_img, valid_label, test_img, test_label = importImages()
+    print(train_img[0][0][0].shape)
+    train_features = []
+    valid_features = []
+    test_features = []
+    for i in range (len(train_img)):
+        for j in range(len(train_img[i])):
+            x = []
+            for k in range(len(train_img[i][j])):
+                x.append(alexNet(train_img[i][j][k].unsqueeze(0)))
+            train_features.append(x)
+    torch.save(train_features, 'train_features.pt')
+    for i in range (len(valid_img)):
+        for j in range(len(valid_img[i])):  
+            x = []
+            for k in range(len(valid_img[i][j])):
+                x.append(alexNet(valid_img[i][j][k].unsqueeze(0)))
+            valid_features.append(x)
+    torch.save(valid_features, 'valid_features.pt')
+    for i in range (len(test_img)):
+        for j in range(len(test_img[i])): 
+            x = []
+            for k in range(len(test_img[i][j])):
+                x.append(alexNet(test_img[i][j][k].reshape([1, 3, 224, 224])))
+            test_features.append(x)
+    torch.save(test_features, 'test_features.pt')
 
   
-  
- class SmallNet(nn.Module):
-  def __init__(self):
-    super(SmallNet, self).__init__()
-    self.name = "small"
-    self.conv = nn.Conv2d(256, 512, 3)
-    self.pool = nn.MaxPool2d(2, 2)
-    self.fc = nn.Linear(128 * 2 * 2, 9)
-  def forward(self, x):
-    x = self.pool(F.relu(self.conv(x)))
-    x = self.pool(x)
-    x = x.view(-1, 128 * 2 * 2)
-    x = self.fc(x)
-    x = x.squeeze(1) # Flatten to [batch_size]
-    return x
+class SmallNet(nn.Module):
+    def __init__(self):
+        super(SmallNet, self).__init__()
+        self.name = "small"
+        self.conv = nn.Conv2d(256, 512, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc = nn.Linear(128 * 2 * 2, 9)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv(x)))
+        x = self.pool(x)
+        x = x.view(-1, 128 * 2 * 2)
+        x = self.fc(x)
+        x = x.squeeze(1) # Flatten to [batch_size]
+
+        return x
+print("START")
+
+get_features()
+
+print("DONE")
+
+
